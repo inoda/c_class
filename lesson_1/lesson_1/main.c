7445eb865c7975f4eebf9ff0db3b0523274b2610
Isaac Noda <isaacnoda@gmail.com>
2018-05-25 09:16:50 -0600

    Fixes spacing

--- lesson_1/lesson_1/main.c
+++ lesson_1/lesson_1/main.c
@@ -9,23 +9,24 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    const char * fileLocation = argv[1];
-    const char filePermission[2] = "r\0";
+  const char * fileLocation = argv[1];
+  const char filePermission[2] = "r\0";
     
-    // Try to open file
-    FILE *fp = fopen(fileLocation, filePermission);
-    if (fp == NULL)
-    {
-        printf("No file found for location %s and permission %s\n", fileLocation, filePermission);
-        return 1;
-    } else {
-        printf("File %s found\n", fileLocation);
-        int status = fclose(fp);
-        if (status != 0) {
-            printf("Failed to close file\n");
-            return 1;
-        }
+  // Try to open file
+  FILE *fp = fopen(fileLocation, filePermission);
+  if (fp == NULL) {
+      printf("No file found for location %s and permission %s\n", fileLocation, filePermission);
+      return 1;
+  } else {
+    printf("File %s found\n", fileLocation);
+
+    // Close file
+    int status = fclose(fp);
+    if (status != 0) {
+      printf("Failed to close file\n");
+      return 1;
     }
-    
-    return 0;
+  }
+
+  return 0;
 }
