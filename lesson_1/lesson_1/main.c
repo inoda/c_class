c5a1aa94c2ede4539d847a03ba7aef038cadaccb
Isaac Noda <isaacnoda@gmail.com>
2018-05-25 07:57:05 -0600

    Initial commit

---
+++ lesson_1/lesson_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lesson_1
+//
+//  Created by Isaac Noda on 5/25/18.
+//  Copyright © 2018 Isaac Noda. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
