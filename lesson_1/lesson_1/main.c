83c6167756249ecbe3f9fd0184d07295dadd7f76
Isaac Noda <isaacnoda@gmail.com>
2018-05-25 17:08:25 -0600

    cleanup

--- lesson_1/lesson_1/main.c
+++ lesson_1/lesson_1/main.c
@@ -13,65 +13,56 @@
 
 int main(int argc, const char * argv[]) {
   const char * file_location = argv[1];
-  const char file_permission[2] = "r\0";
+  const char mode[2] = "r";
     
   // Try to open file
-  FILE *fp = fopen(file_location, file_permission);
+  FILE *fp = fopen(file_location, mode);
   if (fp == NULL) {
-      printf("No file found for location %s and permission %s\n", file_location, file_permission);
+      printf("No file found for location %s and mode %s\n", file_location, mode);
       return 1;
-  } else {
-    printf("File %s found\n", file_location);
+  }
 
-    // Read lines
-    int size = 1000;
-    char row[size];
-    const char delimiter[2] = ",\0";
-    const char *header_indicator = "Name";
+  // Read rows
+  char row[1000];
+  int row_count = 0;
+  while (fgets(row, 1000, fp) != NULL) {
+    row_count += 1;
+    
+    // Skip header
+    if (row_count == 1) { continue; }
 
-    while (fgets(row, size, fp) != NULL) {
-      char *col;
-      char *rp = row;
-      char cols[29][1000];
-
-      // Split row into columns
-      int index = 0;
-      while((col = strsep(&rp, delimiter)) != NULL) {
-        strcpy(cols[index], col);
-        index += 1;
-      }
-      
-      // Skip header
-      int is_header = 0;
-      if (strcmp(cols[0], header_indicator) == 0) {
-        is_header = 1;
-      }
-      if (is_header == 1) {
-        continue;
-      }
-      
-      // Normalize name
-      char *last_char = &cols[0][(strlen(cols[0])-1)];
-      if (strcmp(last_char, "*") == 0 || strcmp(last_char, "#") == 0) {
-        *last_char = '\0';
-      }
-
-      // Load data into struct
-      struct player p;
-      strcpy(p.name, cols[0]);
-      p.at_bats = atoi(cols[6]);
-      p.base_on_balls = atoi(cols[15]);
-      p.strikeouts = atoi(cols[16]);
-      printf("%s, %d, %.1f%%\n", p.name, epa(p), strikeout_percentage(p));
-      printf("\n");
+    // Split row into columns
+    char *col;
+    char *rp = row;
+    char cols[29][1000];
+    int cols_index = 0;
+    while((col = strsep(&rp, ",")) != NULL) {
+      strcpy(cols[cols_index], col);
+      cols_index += 1;
     }
-  
-    // Close file
-    int status = fclose(fp);
-    if (status != 0) {
-      printf("Failed to close file\n");
-      return 1;
+    
+    // Normalize name
+    char *last_char = &cols[0][(strlen(cols[0])-1)];
+    if (strcmp(last_char, "*") == 0 || strcmp(last_char, "#") == 0) {
+      *last_char = '\0';
     }
+
+    // Load data into struct
+    struct player p;
+    strcpy(p.name, cols[0]);
+    p.at_bats = atoi(cols[6]);
+    p.base_on_balls = atoi(cols[15]);
+    p.strikeouts = atoi(cols[16]);
+
+    // Print stats
+    printf("%s, %d, %.1f%%\n", p.name, epa(p), strikeout_percentage(p));
+  }
+
+  // Close file
+  int status = fclose(fp);
+  if (status != 0) {
+    printf("Failed to close file\n");
+    return 1;
   }
 
   return 0;
