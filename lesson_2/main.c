51c731390e693905ff43f97529591017d8ae0043
Isaac Noda <isaacnoda@gmail.com>
2018-06-04 10:05:52 -0600

    Move row -> struct logic into separate func

--- lesson_2/main.c
+++ lesson_2/main.c
@@ -3,6 +3,45 @@
 #include <stdlib.h>
 #include "player.h"
 
+int load_player_data(char *row, struct player *p) {
+  char *col;
+  int cols_index = 0;
+  while((col = strsep(&row, ",")) != NULL) {
+    switch(cols_index) {
+      case 0: {
+        // Normalize name
+        char *last_char = &col[strlen(col)-1];
+        if (*last_char == '*' || *last_char == '#') {
+          *last_char = '\0';
+        }
+        strcpy(p->name, col);
+      }
+      case 2:
+        strcpy(p->team, col);
+      case 6:
+        p->at_bats = atoi(col);
+      case 8:
+        p->hits = atoi(col);
+      case 9:
+        p->doubles = atoi(col);
+      case 10:
+        p->triples = atoi(col);
+      case 11:
+        p->home_runs = atoi(col);
+      case 15:
+        p->base_on_balls = atoi(col);
+      case 16:
+        p->strikeouts = atoi(col);
+      case 28:
+        strcpy(p->positions, col);
+    }
+
+    cols_index += 1;
+  }
+
+  return 0;
+}
+
 int main(int argc, const char * argv[]) {
   const char *file_location = argv[1];
   const char mode[2] = "r";
@@ -31,43 +70,10 @@ int main(int argc, const char * argv[]) {
     //   *players = realloc(*players, sizeof(*players) + (10 * sizeof(struct player *)));
     // }
 
-    // Split row and load data into struct
-    char *col;
+    // Load row into struct
     char *rp = row;
-    int cols_index = 0;
     struct player *p = malloc(sizeof(struct player));
-    while((col = strsep(&rp, ",")) != NULL) {
-      switch(cols_index) {
-        case 0: {
-          // Normalize name
-          char *last_char = &col[strlen(col)-1];
-          if (*last_char == '*' || *last_char == '#') {
-            *last_char = '\0';
-          }
-          strcpy(p->name, col);
-        }
-        case 2:
-          strcpy(p->team, col);
-        case 6:
-          p->at_bats = atoi(col);
-        case 8:
-          p->hits = atoi(col);
-        case 9:
-          p->doubles = atoi(col);
-        case 10:
-          p->triples = atoi(col);
-        case 11:
-          p->home_runs = atoi(col);
-        case 15:
-          p->base_on_balls = atoi(col);
-        case 16:
-          p->strikeouts = atoi(col);
-        case 28:
-          strcpy(p->positions, col);
-      }
-
-      cols_index += 1;
-    }
+    load_player_data(rp, p);
 
     // Print stats
     printf("%s, %s, %d, %d, %d, %d, %d, %d, %d, %s\n", p->name, p->team, p->at_bats, p->hits, p->doubles, p->triples, p->home_runs, p->base_on_balls, p->strikeouts, p->positions);
